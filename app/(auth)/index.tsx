acb704376aa7b950314e4e4186d77828473cd9f8
dfkuro <izmirreffi@gmail.com>
2024-07-02 00:43:42 -0600

    feat(login): testing tamagui
    Just a simple test for tamagui ui

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import { useNavigation } from "expo-router";
-import { View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Button } from "tamagui";
+import { Button, View, Image } from "tamagui";
 
 export default function Index() {
   const navigation = useNavigation();
@@ -12,12 +11,27 @@ export default function Index() {
   }, [navigation]);
 
   return (
-    <>
-      <StatusBar style="dark" backgroundColor="pink" />
-      <View style={{ height: "100%", backgroundColor: "pink" }}>
-        <Text>Hello there</Text>
-        <Button>Hello</Button>
-      </View>
-    </>
+    // <View p={10} bg="pink" flex={1} justifyContent="center">
+    <View
+      style={{
+        padding: 10,
+        backgroundColor: "purple",
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <StatusBar style="light" backgroundColor="purple" />
+      <Image
+        mb={50}
+        alignSelf="center"
+        source={{
+          uri: "https://cdn.pixabay.com/photo/2020/09/16/18/39/icon-5577198_1280.png",
+        }}
+        width={300}
+        height={300}
+      />
+      {/* <Text color="red">Hello there my friend</Text> */}
+      <Button>Log In</Button>
+    </View>
   );
 }
