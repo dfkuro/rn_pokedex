fa26477cb1339551908e229e56f72dbf921af735
izmirreffi@gmail.com <izmirreffi@gmail.com>
2025-01-07 18:00:25 -0600

    feat: Tamagui setup added
    - Added providers to use tamagui themes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,54 +1,44 @@
-import { TamaguiProvider, createTamagui } from "tamagui";
-import { config } from "@tamagui/config/v3";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useColorScheme } from "@/components/useColorScheme";
+import { useColorScheme } from 'react-native'
+import { TamaguiProvider } from "tamagui";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import { SafeAreaView } from "react-native-safe-area-context";
+import tamaguiConfig from '../tamagui.config'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-const tamaguiConfig = createTamagui(config);
-
-type Conf = typeof tamaguiConfig;
-declare module "tamagui" {
-  interface TamaguiCustomConfig extends Conf { }
-}
 
 export default function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
+    if (loaded) SplashScreen.hideAsync();
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+  if (!loaded) return null;
+
 
   return (
-    <TamaguiProvider config={tamaguiConfig}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <SafeAreaView style={{ flex: 1 }}>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            {/* <Stack.Screen name="modal" options={{ presentation: "modal" }} /> */}
-          </Stack>
-        </SafeAreaView>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
       </ThemeProvider>
     </TamaguiProvider>
   );
