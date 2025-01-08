9ab7348a81808de874837b2efd9bad662afa6600
dfkuro <izmirreffi@gmail.com>
2025-01-07 18:05:28 -0600

    Revert "Feat/tamagui setup"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,45 +1,82 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useColorScheme } from 'react-native'
-import { TamaguiProvider } from "tamagui";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { TamaguiProvider, createTamagui } from "tamagui";
+// import {
+//   DarkTheme,
+//   DefaultTheme,
+//   ThemeProvider,
+// } from "@react-navigation/native";
+// import { useColorScheme } from "@/components/useColorScheme";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import tamaguiConfig from '../tamagui.config'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { config } from "@tamagui/config/v3";
+
+const tamaguiConfig = createTamagui(config);
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(auth)/login",
+};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-
-export default function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  const [loaded] = useFonts({
-    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
   });
 
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
-    if (loaded) SplashScreen.hideAsync();
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
   }, [loaded]);
 
-  if (!loaded) return null;
+  if (!loaded) {
+    return null;
+  }
 
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  // const colorScheme = useColorScheme();
 
   return (
-    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <TamaguiProvider>
+      <SafeAreaView style={{ flex: 1 }}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         </Stack>
-      </ThemeProvider>
+      </SafeAreaView>
     </TamaguiProvider>
   );
+
+  // return (
+  //  <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+  //    <SafeAreaView style={{ flex: 1 }}>
+  //      <Stack>
+  //        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+  //      </Stack>
+  //      {/* <Stack> */}
+  //      {/*   <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+  //      {/*   <Stack.Screen name="modal" options={{ presentation: 'modal' }} /> */}
+  //      {/* </Stack> */}
+  //   </SafeAreaView>
+  //  </ThemeProvider>
+  //);
 }
