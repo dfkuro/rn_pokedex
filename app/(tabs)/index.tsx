33b5719cec28cab9437c6ab1c7e47a43ca566def
izmirreffi@gmail.com <izmirreffi@gmail.com>
2025-01-14 20:46:57 -0600

    chore: experiment with tanstack qyery implementation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,33 @@
 // import EditScreenInfo from "@/components/EditScreenInfo";
+import { useQuery } from '@tanstack/react-query';
 import { StyleSheet } from "react-native";
 import { View, Text, Button, YStack, XStack, Theme, H5 } from 'tamagui'
+import { fetchPokemons } from '@/lib/api'
 
 
 export default function TabOneScreen() {
+  const { data } = useQuery<any[]>({
+    queryKey: ['stream-hydrate-pokemons'],
+    queryFn: () => fetchPokemons(),
+    // suspense: true,
+    staleTime: 5 * 1000
+  })
+
+  type dataPokemon = {
+    /** The pokemon name */
+    name: string,
+    /** Url string to get detailed data from that pokemon */
+    url: string
+  }
 
   return <XStack fullscreen padding="$size.xl" alignSelf="center" backgroundColor="$bg">
     <YStack>
-      <Text>Hello</Text>
+      <Text style={{ fontSize: 22 }}>Hello</Text>
+      <View style={{ paddingVertical: 20 }}>
+        {data?.results.map((item: dataPokemon) => {
+          return <Text key={item.name}>{item.name}</Text>
+        })}
+      </View>
     </YStack>
   </XStack>
 
