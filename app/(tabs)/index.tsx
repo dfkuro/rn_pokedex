e15286b963d484550274b4fd0176b55a1b6463ab
izmirreffi@gmail.com <izmirreffi@gmail.com>
2025-02-17 14:28:59 -0600

    chore: basic config for consume api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,42 +1,68 @@
+import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { StyleSheet, View, Text } from "react-native";
-import { fetchPokemons } from '@/lib/api'
+import { fetchPokemon } from '@/lib/api'
+// import { Image } from 'expo-image'
 
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+const tilListPokemons = 151;
+
+interface Pokemon {
+  /** The pokemon name */
+  name: string;
+  /** Url string to get detailed data from that pokemon */
+  url: string;
+}
+
+interface PokemonApiResponse {
+  /** Total pokemons */
+  count: number;
+  next: string;
+  previous: string | null;
+  /** Array of pokemons */
+  results: Pokemon[];
+}
+
+interface PokemonDetails {
+  name: string;
+  sprites: {
+    front_default: string;
+  }
+}
 
 export default function TabOneScreen() {
-	const { data } = useQuery<any[]>({
-		queryKey: ['stream-hydrate-pokemons'],
-		queryFn: () => fetchPokemons(),
-		// suspense: true,
-		staleTime: 5 * 1000
-	})
+  const [pokemons, setPokemons] = useState<PokemonDetails[]>([]);
 
-	type dataPokemon = {
-		/** The pokemon name */
-		name: string,
-		/** Url string to get detailed data from that pokemon */
-		url: string
-	}
+  const { data: pokemonDetails } = useQuery<PokemonApiResponse>({
+    queryKey: ['pokemons'],
+    queryFn: () => fetchPokemon(1),
+    // suspense: true,
+    staleTime: 5 * 1000
+  })
 
-	return <View style={{ flex: 1, height: '200', padding: 10 }}>
-		<Text style={{ fontSize: 22 }}>Hello</Text>
-		<View style={{ paddingVertical: 20 }}>
-			{data?.results.map((item: dataPokemon) => {
-				return <Text style={styles.textStyle} key={item.name}>{item.name}</Text>
-			})}
-		</View>
-	</View>
+
+  return <View style={{ flex: 1, padding: 10 }}>
+    <Text style={{ fontSize: 22 }}>Hello</Text>
+    <View style={{ paddingVertical: 20 }}>
+      {/* {pokemons?.map((item: Pokemon) => { */}
+      {/*   return <View key={item.name}> *
+      {/*     {/* <Image source={`item.`} /> */}
+      {/*     <Text style={styles.textStyle}>{item.name}</Text> */}
+      {/*   </View> */}
+      {/* })} */}
+    </View>
+  </View>
 
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-		height: 200,
-		backgroundColor: 'pink'
-	}
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 200,
+    backgroundColor: 'pink'
+  },
+  textStyle: {
+    color: 'white'
+  }
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,12 @@ import { DevToolsBubble } from "react-native-react-query-devtools";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import * as  Clipboard from 'expo-clipboard';
 import {
-	DarkTheme,
-	DefaultTheme,
-	ThemeProvider,
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
 } from "@react-navigation/native";
 import { useColorScheme } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -18,66 +19,41 @@ SplashScreen.preventAutoHideAsync();
 
 
 export default function RootLayoutNav() {
-	const colorScheme = useColorScheme();
-	const queryClient = new QueryClient();
+  const colorScheme = useColorScheme();
+  const queryClient = new QueryClient();
 
-	const onCopy = async (text: string) => {
-		try {
-			// This words for expo
-			await Clipboard.setStringAsync(text)
-			return true
-		} catch {
-			return true
-		}
-	}
+  const onCopy = async (text: string) => {
+    try {
+      // This words for expo
+      await Clipboard.setStringAsync(text)
+      return true
+    } catch {
+      return true
+    }
+  }
 
-	const [loaded] = useFonts({
-		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-		...FontAwesome.font,
-	});
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
 
-	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
-	useEffect(() => {
-		if (error) throw error;
-	}, [error]);
+  useEffect(() => {
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded]);
 
-	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync();
-		}
-	}, [loaded]);
+  if (!loaded) return null;
 
-	if (!loaded) {
-		return null;
-	}
 
-	return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-	// const colorScheme = useColorScheme();
-
-	return (
-		<QueryClientProvider client={queryClient}>
-			<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-				<Stack>
-					<Stack.Screen name="(auth)" options={{ headerShown: false }} />
-				</Stack>
-			</ThemeProvider>
-		</TamaguiProvider>
-	);
-
-	// return (
-	//  <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-	//    <SafeAreaView style={{ flex: 1 }}>
-	//      <Stack>
-	//        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-	//      </Stack>
-	//      {/* <Stack> */}
-	//      {/*   <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
-	//      {/*   <Stack.Screen name="modal" options={{ presentation: 'modal' }} /> */}
-	//      {/* </Stack> */}
-	//   </SafeAreaView>
-	//  </ThemeProvider>
-	//);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
+          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
+        <StatusBar style={colorScheme || 'dark'} />
+      </ThemeProvider>
+      <DevToolsBubble onCopy={onCopy} />
+    </QueryClientProvider>
+  );
 }
